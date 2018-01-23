f02938a4dd789039d4d006cb68e28f98b187adc8
Quentin Geissmann <qgeissmann@gmail.com>
2018-01-23 17:11:25 +0000

    fixes #19

--- tests/testthat/test-geom_peak.R
+++ tests/testthat/test-geom_peak.R
@@ -7,4 +7,12 @@ test_that("geom_peak works", {
                       facet_wrap( ~ id, labeller = id_labeller)
   out
   out + geom_peak(colour="black")
+
+
+  # with default values for aes
+  out <- ggperio(per_dt) +
+    stat_pop_etho() +
+    facet_wrap( ~ id, labeller = id_labeller)+
+   geom_peak(colour="black")
+  out
 })
